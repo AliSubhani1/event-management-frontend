df9169f44ec268879b0db9fade9586dfc8e9d9e3
Ali <ali.subhani@tintash.com>
2024-11-10 23:45:32 +0200

    Add transitions on search events page

--- src/Pages/Home/SearchEvents.tsx
+++ src/Pages/Home/SearchEvents.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import useDebounce from '../../CustomHooks/UseDebounc'; 
 import SearchImage from '../../Assets/svg/search.svg'
+import EventContainer from './EventContainer';
 
 const SearchEvents = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [isVisible, setIsVisible] = useState(false);
   // ToDo: Update typescript type here later when real API will be used
   const [events, setEvents] = useState<any[]>([]);
 
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
 
   useEffect(() => {
       fetchEvents(debouncedSearchTerm);
@@ -27,12 +33,16 @@ const SearchEvents = () => {
 
   return (
     <>
-    <div className="bg-blue-0 p-6 w-screen ">
+    <div
+        className={`bg-blue-0 p-6 sm:w-[95vw] mx-auto rounded-md mt-12 transform transition-transform duration-700 ease-in-out ${
+          isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'
+        }`}
+      >
         <div className='text-left text-white-0'>
             <h3 className="text-2xl font-bold">Events</h3>
             <p className="text-gray-700 mb-4">Book the ticket of ongoing events</p>
         </div>
-<div className='flex justify-center'>
+    <div className='flex justify-center'>
       <input
         type="text"
         value={searchTerm}
@@ -40,30 +50,29 @@ const SearchEvents = () => {
         placeholder="Search events..."
         className="border p-2 w-full mb-4 max-w-[400px] rounded-sm text-black"
       />
-      </div>
     </div>
-          <div>
+    </div>
+        <div>
           {events.length > 0 ? (
-            <ul>
+            <div className='flex gap-8 flex-wrap items-center justify-center'>
               {events.map((event, index) => (
-                <>
-                <p key={index} className="border-b py-2">
-                  {event.title} 
-                </p>
-                <p>{event.description}</p>
-                <img className='w-[80px]' src={event.images[0]} alt="" />
-                </>
+                <EventContainer eventName={event.title} image={event.images[0]}/>
               ))}
-            </ul>
-          ) : (
-            <div className='flex mt-8 flex-col items-center justify-center gap-4'>
-                <img src={SearchImage} alt="" />
-                <h3 className='text-xl font-bold'>No results found</h3>
-                <p className='text-center'>OOPS! We couldn't found any results. Please try again later.</p>
-                
             </div>
-
-          )}
+          ) : (
+            searchTerm !== '' && (
+              <div
+              className={`flex mt-8 flex-col items-center justify-center gap-4 transform transition-transform duration-700 ease-in-out ${
+                isVisible ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
+              }`}
+            >
+                        <img src={SearchImage} alt="" />
+                        <h3 className='text-xl font-bold'>No results found</h3>
+                        <p className='text-center'>OOPS! We couldn't found any results. Please try again later.</p>
+                        
+                    </div>
+            ) )
+          }          
         </div>
         </>
   );
