fcd2c0545f30e4cd9a3a6c4060af97cb6ff0975f
Ali <ali.subhani@tintash.com>
2024-11-10 17:20:20 +0200

    wrap all routes with browser router and add new navbar section in the app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import './App.css';
 import { persistor, store } from './Store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import TopBar from './Components/Common/TopBar';
+import Navbar from './Components/Common/Navbar/index';
 
 function App() {
   return (
@@ -11,8 +13,11 @@ function App() {
       <header className="App-header">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/">
           <TopBar />
+          <Navbar />
           <Home />
+          </BrowserRouter>
         </PersistGate>
       </Provider>
       </header>
