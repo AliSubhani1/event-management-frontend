9f8e027b2b9f2f007f149dc227afb6f78dbc5d82
Ali <ali.subhani@tintash.com>
2024-11-10 22:23:18 +0200

    update styling for topbar

--- src/Components/Common/TopBar.tsx
+++ src/Components/Common/TopBar.tsx
@@ -2,12 +2,14 @@ import { FaCrown } from "react-icons/fa";
 
 const TopBar = () => {
     return (
-      <div className="bg-blue-0 px-4 py-2 fixed top-0 left-0 z-50 w-full text-base">
+      <>
+      <div className="bg-blue-0 px-4 py-2 z-50 w-full text-base">
         <div className="w-full flex gap-2 items-center justify-center">
         <FaCrown className="text-yellow-0 size-5"/>
-        <p>Get access to your favourite events</p>
+        <p className="text-white-0">Get access to your favourite events</p>
         </div>
       </div>
+      </>
     );
   };
   
