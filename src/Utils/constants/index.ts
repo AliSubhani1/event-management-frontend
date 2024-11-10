0b85fc35a017f597e35aeab2972d378db1b1ebc6
Ali <ali.subhani@tintash.com>
2024-11-10 17:22:13 +0200

    add new navbar items to constants file

--- src/Utils/constants/index.ts
+++ src/Utils/constants/index.ts
@@ -1,11 +1,20 @@
 export enum ButtonColor {
-    DARK_BLUE = 'text-white-0 bg-blue-1 hover:opacity-80 border border-blue-1',
-    DARK_BLUE_2 = 'text-white-0 bg-blue-1 border border-blue-1',
+    DARK_BLUE = 'text-white bg-blue-0 border border-blue-0 hover:opacity-90',
     White = 'text-blue-1 bg-white-0 hover:opacity-80 border border-blue-1',
     Red = 'text-red-0 bg-white-0 hover:opacity-80 border border-red-0',
   }
 
-  export enum Navigation {
-    DEFAULT = '/',
-    DASHBOARD = '/dashboard',
+  export enum Pages {
+    Home = '/',
+    About = '/about',
+    ContactUs = '/contact-us',
+    SignUp = '/signup',
+    Login = '/login',
   }
+
+
+  export const navbarItems = [
+    {title: 'Home', path: Pages.Home},
+    {title: 'About', path: Pages.About},
+    {title: 'Contact Us', path: Pages.ContactUs},
+]
