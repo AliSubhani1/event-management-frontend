e375f81f5617456758634e2e4750d1fc3749a63c
Ali <ali.subhani@tintash.com>
2024-11-10 22:24:16 +0200

    Update styling for dark blue button type in constants

--- src/Utils/constants/index.ts
+++ src/Utils/constants/index.ts
@@ -1,5 +1,5 @@
 export enum ButtonColor {
-    DARK_BLUE = 'text-white bg-blue-0 border border-blue-0 hover:opacity-90',
+    DARK_BLUE = 'text-white-0 bg-blue-0 border border-blue-0 hover:opacity-90',
     White = 'text-blue-1 bg-white-0 hover:opacity-80 border border-blue-1',
     Red = 'text-red-0 bg-white-0 hover:opacity-80 border border-red-0',
   }
