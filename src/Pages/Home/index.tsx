5f3d7747b4ac5829643158030fea9294d7300421
Ali <ali.subhani@tintash.com>
2024-11-11 18:33:06 +0200

    Add event partners component to show on home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import SearchEvents from "./SearchEvents";
+import EventPartners from "./EventPartners";
 
 const Home = () =>{
     return (
         <div>
             <SearchEvents />
+            <EventPartners />
         </div>
     )
 }
