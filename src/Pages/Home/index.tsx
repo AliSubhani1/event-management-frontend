e85a6180fc662d7f4f2ec4afa0e77b4183ace769
Ali <ali.subhani@tintash.com>
2024-11-24 01:15:53 +0200

    Update home page to include newly created sections

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import SearchEvents from "./SearchEvents";
 import EventPartners from "./EventPartners";
+import WhyUseIt from "../../Components/Common/WhyUseIt";
 
 const Home = () =>{
     return (
         <div>
             <SearchEvents />
+            <WhyUseIt />
             <EventPartners />
         </div>
     )
