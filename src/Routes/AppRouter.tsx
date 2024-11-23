c5500297f4bf9b6bc96793a148ffe531444432f8
Ali <ali.subhani@tintash.com>
2024-11-24 01:16:15 +0200

    Update app router component to add all events route

--- src/Routes/AppRouter.tsx
+++ src/Routes/AppRouter.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import { Pages } from '../Utils/constants';
 import Home from '../Pages/Home';
+import AllEvents from '../Pages/AllEvents';
 const AppRouter = () => (
     <Routes>
       <Route
         path={Pages.Home}
         element={
             <Home />
+        }
+      />
+      <Route
+        path={Pages.AllEvents}
+        element={
+            <AllEvents />
         }
       />
       </Routes>
