eaa3c10661e494118797014fb5defff0f8aa542d
Ali <ali.subhani@tintash.com>
2024-11-25 12:43:15 +0200

    Add the new components to hoem page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,13 +1,17 @@
 import SearchEvents from "./SearchEvents";
 import EventPartners from "./EventPartners";
 import WhyUseIt from "../../Components/Common/WhyUseIt";
+import InformationSection from "./InformationSection";
+import FAQSection from "../../Components/Common/Faqs";
 
 const Home = () =>{
     return (
         <div>
             <SearchEvents />
             <WhyUseIt />
+            <InformationSection />
             <EventPartners />
+            <FAQSection />
         </div>
     )
 }
