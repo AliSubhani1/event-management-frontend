2c44dce7e873b9835a014f21fb335d68e8faa270
Ali <ali.subhani@tintash.com>
2024-11-25 12:43:37 +0200

    Update app router to show routes for newly added pages

--- src/Routes/AppRouter.tsx
+++ src/Routes/AppRouter.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Pages } from '../Utils/constants';
 import Home from '../Pages/Home';
 import AllEvents from '../Pages/AllEvents';
+import Contact from '../Pages/Contact';
+
 const AppRouter = () => (
     <Routes>
       <Route
@@ -14,6 +16,12 @@ const AppRouter = () => (
         path={Pages.AllEvents}
         element={
             <AllEvents />
+        }
+      />
+      <Route
+        path={Pages.ContactUs}
+        element={
+            <Contact />
         }
       />
       </Routes>
