7e8258033cce732f05ae9d25436f611d784ad70a
Ali <ali.subhani@tintash.com>
2024-11-25 12:42:27 +0200

    Create a reuseable component for image text box

--- src/Components/Common/ImageTextBox.tsx
+++ src/Components/Common/ImageTextBox.tsx
@@ -1,19 +1,22 @@
 type ImageTextBoxProps = {
-    icon: string,
-    heading: string, 
-    description: string,
-}
+    icon: string;
+    heading: string;
+    description: string;
+};
 
-const ImageTextBox = ({icon, heading, description} : ImageTextBoxProps) => {
+const ImageTextBox = ({ icon, heading, description }: ImageTextBoxProps) => {
     return (
-        <div>
+        <div
+            className="border-gray-0 border-opacity-40 border rounded-md flex p-4 gap-4 justify-center w-[360px] cursor-pointer 
+                       hover:shadow-lg hover:-translate-y-2 transition-all duration-300 ease-in-out"
+        >
             <img src={icon} alt="icon" />
             <div>
-                <h3>{heading}</h3>
+                <h3 className="text-xl">{heading}</h3>
                 <p>{description}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ImageTextBox;
