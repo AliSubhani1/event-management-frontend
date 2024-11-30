568af9cd0cf1744518b1ab40e85a7c5e295a16f2
Ali <ali.subhani@tintash.com>
2024-11-30 15:24:25 +0200

    update app router to include new router for event details

--- src/Routes/AppRouter.tsx
+++ src/Routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Pages } from '../Utils/constants';
 import Home from '../Pages/Home';
 import AllEvents from '../Pages/AllEvents';
 import Contact from '../Pages/Contact';
+import EventDetails from '../Pages/EventDetails';
 
 const AppRouter = () => (
     <Routes>
@@ -22,6 +23,12 @@ const AppRouter = () => (
         path={Pages.ContactUs}
         element={
             <Contact />
+        }
+      />
+      <Route
+        path={Pages.EventDetails}
+        element={
+            <EventDetails />
         }
       />
       </Routes>
