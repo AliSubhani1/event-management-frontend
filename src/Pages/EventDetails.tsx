850f84bd192487a7d1a77e19f80f83997ab6bd5e
Ali <ali.subhani@tintash.com>
2024-12-02 12:19:13 +0200

    Add updates to eventd details page

--- src/Pages/EventDetails.tsx
+++ src/Pages/EventDetails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import jsPDF from "jspdf";
 import CustomButton from "../Components/Common/CustomButton";
@@ -7,6 +7,7 @@ import { ButtonColor } from "../Utils/constants";
 const EventDetails: React.FC = () => {
   const location = useLocation();
   const event = location.state?.event;
+  const [showMessage, setShowMessage] = useState<boolean>(false);
 
   if (!event) {
     return (
@@ -25,35 +26,51 @@ const EventDetails: React.FC = () => {
     pdf.text("Event Ticket", 10, 20);
 
     pdf.setFontSize(20);
-    pdf.text(event.title, 10, 40);
+    pdf.text(event.name, 10, 40);
 
     // Add the event description
     pdf.setFontSize(12);
     const descriptionText = pdf.splitTextToSize(event.description, 180); // Split long text for the page width
     pdf.text(descriptionText, 10, 60);
 
+    pdf.text(`${event.price} EUR`, 10, 80);
+
+    pdf.text("Show this ticket on venue to claim your reservation. You will have to pay at the venue.", 10, 100);
+
     // Add the event image
     const img = new Image();
-    img.src = event.images[0];
+    img.src = event.image;
     img.onload = () => {
-      pdf.addImage(img, "JPEG", 10, 80, 180, 100); // Position: x=10, y=60; Size: width=180, height=100
-      pdf.save(`${event.title}_ticket.pdf`);
+      pdf.addImage(img, "JPEG", 10, 120, 180, 100); // Position: x=10, y=60; Size: width=180, height=100
+      pdf.save(`${event.name}_ticket.pdf`);
     };
+    setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 8000);
   };
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">{event.title}</h1>
+    <div className="p-6 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold my-8">{event.name}</h1>
       <img
-        src={event.images[0]}
-        alt={event.title}
-        className="w-full max-w-md mx-auto mb-4"
+        src={event.image}
+        alt={event.name}
+        className="w-full max-w-md mb-4"
       />
       <p className="text-gray-600 mb-4">{event.description}</p>
+      <p className="text-gray-600 mb-4">{`${event.price} EUR`}</p>
       <div className="flex w-full items-center justify-center">
-        <CustomButton onClick={downloadTicket} className='mr-4 my-8' colorClass={ButtonColor.DARK_BLUE} >Download Ticket</CustomButton>
+        <CustomButton onClick={downloadTicket} className='my-8' colorClass={ButtonColor.DARK_BLUE} >Reserve Ticket</CustomButton>
       </div>
-
+      {showMessage && (
+        <div className="text-center mt-4 p-4 border border-green-0 rounded">
+          <p className="text-green-0 font-bold">
+            Your ticket has been downloaded. Show this ticket at the event venue
+            to claim your reservation.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
